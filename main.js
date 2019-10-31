f72072b4eb5fc334bf8d819d8bfebbc0183cf99e
tyelf22 <tyelf22@hotmail.com>
2019-10-30 23:15:23 -0600

    added nav bar

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("tyson connected!");
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+burger.addEventListener('click', () => {
+    console.log('You pushed me')
+    nav.classList.toggle('nav-active');
+
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        }
+    });
+
+    burger.classList.toggle('toggle');
+
+  });
+
+  
+
+}
+
+
+navSlide();
